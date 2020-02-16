f3bb3247c704264ac69558ec7d7088182901a569
Ebner Matias Trigo da Silva <ebner.suporte@hotmail.com>
2020-02-16 15:05:21 -0300

    chore: add options to fastfile

--- deploy/decrypt.sh
+++ deploy/decrypt.sh
@@ -2,7 +2,7 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_PASSWORD" \
---output deploy/buyzerapp.keystore deploy/buyzerapp.keystore.gpg
+--output android/app/buyzerapp.keystore deploy/buyzerapp.keystore.gpg
 
 gpg --quiet --batch --yes --decrypt --passphrase="$ENCRYPT_PASSWORD" \
---output deploy/Conta_de_Servico.json deploy/Conta_de_Servico.json.gpg
+--output android/app/key_service_google.json android/app/key_service_google.json.gpg
